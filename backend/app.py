5b8038926cfa4b6a115bd65ff54c21f6a476ec81
Daniil L <daniil.larionov@gmail.com>
2023-11-02 14:08:48 +0300

    baccckkk

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+import pickle
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+with open('../arima2.pkl', 'rb') as pkl:
+    MODEL = pickle.load(pkl)
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return {"status": "OK"}
+
+
+@app.get("/predict")
+def predict(months):
+    # _validate_predict_params(months=months)
+    # модель обчучена на пятидневках
+    predictions = MODEL.predict(int(months)*7)
+    return {"date": str(list(predictions.index)), "predict": str(list(predictions.values))}
